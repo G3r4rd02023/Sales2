d818b0f6c2aaa9baa8af8a4ad29fe159e3567c54
Gerardo Lanza <glanza007@gmail.com>
2023-04-11 11:52:27 -0600

    Api Countries

--- Sales/Sales.API/Services/IApiService.cs
+++ Sales/Sales.API/Services/IApiService.cs
@@ -1,6 +1,9 @@
-﻿namespace Sales.API.Services
+﻿using Sales.Shared.Responses;
+
+namespace Sales.API.Services
 {
     public interface IApiService
     {
+        Task<Response> GetListAsync<T>(string servicePrefix, string controller);
     }
 }
--- Sales/Sales.Shared/Responses/CityResponse.cs
+++ Sales/Sales.Shared/Responses/CityResponse.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
 
 namespace Sales.Shared.Responses
 {
-    internal class CityResponse
+    public class CityResponse
     {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("name")]
+        public string? Name { get; set; }
     }
 }
+
