676973630a165ed2dc3ca3f631fefe7bf0191eed
Gerardo Lanza <glanza007@gmail.com>
2023-04-12 16:00:04 -0600

    Autorization

---
+++ Sales/Sales.Web/Auth/AuthenticationProviderTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using System.Security.Claims;
+
+namespace Sales.WEB.Auth
+{
+    public class AuthenticationProviderTest : AuthenticationStateProvider
+    {
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            await Task.Delay(3000);
+            var anonimous = new ClaimsIdentity();
+            var gerUser = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim("FirstName", "Gerardo"),
+                new Claim("LastName", "Lanza"),
+                new Claim(ClaimTypes.Name, "gerardo@yopmail.com"),
+                new Claim(ClaimTypes.Role, "Admin")
+            },
+            authenticationType: "test");
+            return await Task.FromResult(new AuthenticationState(new ClaimsPrincipal(gerUser)));
+        }
+    }
+}
+
--- Sales/Sales.Web/Program.cs
+++ Sales/Sales.Web/Program.cs
@@ -1,17 +1,20 @@
 using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Sales.Web;
 using Sales.Web.Repositories;
+using Sales.WEB.Auth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7151/") });
+builder.Services.AddSingleton(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7151/") });
 builder.Services.AddScoped<IRepository, Repository>();
 builder.Services.AddSweetAlert2();
-
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, AuthenticationProviderTest>();
 
 
 await builder.Build().RunAsync();
