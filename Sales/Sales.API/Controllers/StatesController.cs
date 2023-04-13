35d66acbb9faaedaff7caaf0d7bda58c76435bbe
Gerardo Lanza <glanza007@gmail.com>
2023-04-13 13:16:58 -0600

    register,login and logout

--- Sales/Sales.API/Controllers/StatesController.cs
+++ Sales/Sales.API/Controllers/StatesController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
 using Sales.API.Helpers;
 using Sales.Shared.DTOs;
 using Sales.Shared.Entities;
+
 
 namespace Sales.API.Controllers
 {
