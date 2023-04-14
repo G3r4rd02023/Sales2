31d972002ba4eb6f1ebca660e163ed035cfd4095
Gerardo Lanza <glanza007@gmail.com>
2023-04-14 14:49:55 -0600

    Categories

--- Sales/Sales.API/Controllers/CategoriesController.cs
+++ Sales/Sales.API/Controllers/CategoriesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Sales.API.Data;
 using Sales.API.Helpers;
@@ -8,20 +10,22 @@ using Sales.Shared.Entities;
 namespace Sales.API.Controllers
 {
     [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("/api/categories")]
-    public class CategoriesController : ControllerBase
+    public class CategoiresController : ControllerBase
     {
         private readonly DataContext _context;
 
-        public CategoriesController(DataContext context)
+        public CategoiresController(DataContext context)
         {
             _context = context;
         }
 
+
         [HttpGet]
-        public async Task<IActionResult> GetAsync([FromQuery] PaginationDTO pagination)
+        public async Task<ActionResult> Get([FromQuery] PaginationDTO pagination)
         {
-            var queryable = _context.Categories                
+            var queryable = _context.Categories
                 .AsQueryable();
 
             if (!string.IsNullOrWhiteSpace(pagination.Filter))
@@ -35,10 +39,12 @@ namespace Sales.API.Controllers
                 .ToListAsync());
         }
 
+
         [HttpGet("totalPages")]
         public async Task<ActionResult> GetPages([FromQuery] PaginationDTO pagination)
         {
-            var queryable = _context.Categories.AsQueryable();
+            var queryable = _context.Categories
+                .AsQueryable();
 
             if (!string.IsNullOrWhiteSpace(pagination.Filter))
             {
@@ -48,6 +54,19 @@ namespace Sales.API.Controllers
             double count = await queryable.CountAsync();
             double totalPages = Math.Ceiling(count / pagination.RecordsNumber);
             return Ok(totalPages);
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult> Get(int id)
+        {
+            var category = await _context.Categories
+                .FirstOrDefaultAsync(x => x.Id == id);
+            if (category is null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
         }
 
 
@@ -64,7 +83,7 @@ namespace Sales.API.Controllers
             {
                 if (dbUpdateException.InnerException!.Message.Contains("duplicate"))
                 {
-                    return BadRequest("Ya existe una categoria con el mismo nombre.");
+                    return BadRequest("Ya existe un registro con el mismo nombre.");
                 }
                 else
                 {
@@ -75,19 +94,6 @@ namespace Sales.API.Controllers
             {
                 return BadRequest(exception.Message);
             }
-
-        }
-
-        [HttpGet("{id:int}")]
-        public async Task<ActionResult> Get(int id)
-        {
-            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
-            if (category is null)
-            {
-                return NotFound();
-            }
-
-            return Ok(category);
         }
 
         [HttpPut]
@@ -114,24 +120,20 @@ namespace Sales.API.Controllers
             {
                 return BadRequest(exception.Message);
             }
-
         }
 
         [HttpDelete("{id:int}")]
-        public async Task<ActionResult> Delete(int id)
+        public async Task<IActionResult> DeleteAsync(int id)
         {
-            var afectedRows = await _context.Categories
-                .Where(x => x.Id == id)
-                .ExecuteDeleteAsync();
-
-            if (afectedRows == 0)
+            var category = await _context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if (category == null)
             {
                 return NotFound();
             }
 
+            _context.Remove(category);
+            await _context.SaveChangesAsync();
             return NoContent();
         }
-
-
     }
 }
